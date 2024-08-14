d759f7816db82100992edca973b26fb428052db9
José Jaramillo <josejara93106@gmail.com>
2024-08-14 16:03:47 +0000

    carta aleatoria, incluido temporizador y botón

--- src/app.js
+++ src/app.js
@@ -6,6 +6,47 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let cardSuits = document.querySelectorAll(".suit");
+  let cardNumbers = document.querySelector(".number");
+
+  function getRandomCard() {
+    let randomSuitIndex = Math.floor(Math.random() * suits.length);
+    let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+
+    const randomSuit = suits[randomSuitIndex];
+    const randomNumber = numbers[randomNumberIndex];
+
+    cardSuits.forEach(suitElement => {
+      suitElement.textContent = randomSuit;
+      if (randomSuit === "♥" || randomSuit === "♦") {
+        suitElement.classList.add("red-suit");
+      } else {
+        suitElement.classList.remove("red-suit");
+      }
+    });
+
+    cardNumbers.textContent = randomNumber;
+  }
+  const newCardButton = document.getElementById("newCardButton");
+
+  newCardButton.addEventListener("click", getRandomCard);
+
+  getRandomCard();
+
+  setInterval(getRandomCard, 10000);
 };
+
+let contador = 9;
+const elementoContador = document.getElementById("contador");
+
+const intervalo = setInterval(() => {
+  elementoContador.textContent = contador;
+  contador--;
+
+  if (contador < 1) {
+    contador = 10; // Reiniciamos el contador a 10
+  }
+}, 1000);
